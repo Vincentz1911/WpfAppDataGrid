7aceed9836f36105e6efd43d29b32ba96cc54ca4
Vincentz1911 <henrik_v_poulsen@hotmail.com>
2018-03-17 12:16:19 +0100

    321

--- WpfAppDataGrid/ViewModels/UserViewModel.cs
+++ WpfAppDataGrid/ViewModels/UserViewModel.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Input;
 //using WpfAppDataGrid.Models;
 
 namespace WpfAppDataGrid.ViewModels
 {
     class UserViewModel
     {
+        public ICommand ButtonCommand { get; set; }
+        public ICommand ButtonCommandSQL { get; set; }
         public ObservableCollection<Models.User> Users_obsColl { get; set; }
         public DataTable Users_datatable { get; set; }
 
@@ -14,6 +19,10 @@ namespace WpfAppDataGrid.ViewModels
 
         public UserViewModel()
         {
+            ButtonCommand = new RelayCommand(new Action<object>(ShowMessage));
+
+            ButtonCommandSQL = new RelayCommand(new Action<object>(InsertDataToDb));
+
             //Load Users from adding them to an Observable Collection
             LoadUsers();
 
@@ -22,6 +31,48 @@ namespace WpfAppDataGrid.ViewModels
 
             //Load Users from SQL database via Datatable into Observable Collection
             LoadDatabaseUsers();
+        }
+
+        public void InsertDataToDb(object obj)
+        {
+            
+            //var records = obj;
+
+            SqlConnection con = new SqlConnection(@"Data Source=(local); Initial Catalog = UserDatabase; Integrated Security = True");
+            con.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO Users VALUES (@param1, @param2)");
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+                //cmd.Parameters.Add("@param1", DbType.String);
+                //cmd.Parameters.Add("@param2", DbType.String);
+                //cmd.Parameters.Add("@param3", DbType.String);
+
+                foreach (var item in Users_obsColl)
+                {
+                    cmd.Parameters[0].Value = item.LoginName;
+                    cmd.Parameters[1].Value = item.Password;
+                    //cmd.Parameters[2].Value = item.param3;
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                con.Close();
+            
+        }
+
+        private void SaveSQL(object obj)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(local); Initial Catalog = UserDatabase; Integrated Security = True");
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT Users VALUES (5, 'NorthWestern')";
+            cmd.Connection = con;
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
 
         void LoadUsers()
@@ -41,7 +92,7 @@ namespace WpfAppDataGrid.ViewModels
             SqlConnection con = new SqlConnection(@"Data Source=(local); Initial Catalog = UserDatabase; Integrated Security = True");
             SqlCommand cmd = new SqlCommand("select * from Users", con);
 
-            con.Open();          
+            con.Open();
             SqlDataAdapter adp = new SqlDataAdapter(cmd);
             Users_datatable = new DataTable();
             adp.Fill(Users_datatable);
@@ -63,7 +114,37 @@ namespace WpfAppDataGrid.ViewModels
             Users_obsColl = users;
         }
 
-
+        public void ShowMessage(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+        }
 
     }
+
+    class RelayCommand : ICommand
+    {
+        private Action<object> _action;
+
+        public RelayCommand(Action<object> action) { _action = action; }
+
+        #region ICommand Members
+        public bool CanExecute(object parameter) { return true; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                _action(parameter);
+            }
+            else
+            {
+                _action("Done!");
+            }
+        }
+
+        #endregion
+    }
+
 }
